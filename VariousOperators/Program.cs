9211545fc819b7693a668f36c5fa4a3dd0996eb9
Isis <mejaikatwt@gmail.com>
2026-02-26 15:13:05 +0000

    Exercício 4

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string inp = Console.ReadLine();
+            byte bt = byte.Parse(inp);
+            Console.WriteLine($"{bt/2}");
+            Console.WriteLine($"{bt<<3}");
+            Console.WriteLine($"{bt^6}");
+            Console.WriteLine($"{bt>10}");
+            
+        }
+    }
+}
