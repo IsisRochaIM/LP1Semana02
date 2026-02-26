d55e797301373e391a3527d3914560dbc1eebd56
Isis <mejaikatwt@gmail.com>
2026-02-26 15:41:51 +0000

    Exercício 5

---
+++ Cilindro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Altura: ");
+            string inpa = Console.ReadLine();
+            double a = Convert.ToDouble(inpa);
+            Console.Write("Raio: ");
+            string inpr = Console.ReadLine();
+            double r = Convert.ToDouble(inpr);
+            Console.WriteLine($"{Math.PI*Math.Pow(r,2)*a}");
+            double s = 2*Math.PI*r*(r+a);
+            Console.WriteLine($"{s:f2}");
+        }
+    }
+}
