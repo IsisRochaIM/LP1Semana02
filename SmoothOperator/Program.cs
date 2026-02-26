b7f11c24a6b264d2342fe108f49a22bb93ae0c52
Isis <mejaikatwt@gmail.com>
2026-02-26 14:30:46 +0000

    Exercico 1

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insere número inteiro: ");
+            string input = Console.ReadLine();
+            int value = int.Parse(input);
+            sbyte n = Convert.ToSByte(value);
+            Console.WriteLine(--n);
+            Console.WriteLine(++n);
+
+        }
+    }
+}
