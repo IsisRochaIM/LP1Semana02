8849537407573b2ed8cb22dba4aee91a3b70e875
Isis <mejaikatwt@gmail.com>
2026-02-26 16:36:31 +0000

    Commit 3

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -10,10 +10,21 @@ namespace TempConvert
             CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
             Console.Write("Temperature: ");
             string inpT = Console.ReadLine();
+            double t = Convert.ToDouble(inpT);
             Console.Write("Unit (C/F): ");
             string inpU = Console.ReadLine();
-            
 
+            switch(inpU)
+            {
+                case "C":
+                    t = t*1.8 + 32;
+                    Console.WriteLine($"{inpT} C = {t:f2} F");
+                    break;
+                case "F":
+                    t = (t - 32)/1.8;
+                    Console.WriteLine($"{inpT} F = {t:f2} C");
+                    break;
+            }
         }
     }
 }
